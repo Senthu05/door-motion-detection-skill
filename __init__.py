cf2b690d857dbc125c6fec82bc25b164b222fa57
Senthuran Puthirarajah <69337831+Senthu05@users.noreply.github.com>
2022-03-22 15:29:11 +1030

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,56 +1,56 @@
 from mycroft import MycroftSkill, intent_file_handler
 # from mycroft.messagebus.message import Message
-# from mycroft.util import play_wav
+from mycroft.util import play_wav
 
-# import time
-# import RPi.GPIO as GPIO
+import time
+import RPi.GPIO as GPIO
 
-# REMINDER_PING = join(dirname(__file__), 'twoBeep.wav')
-# # GPIO pins
-# MOTION = 23
-# LED = 25
-# detect_time = []
-# MOTION_GAP = 60 # second
+REMINDER_PING = join(dirname(__file__), 'twoBeep.wav')
+# GPIO pins
+MOTION = 23
+LED = 25
+detect_time = []
+MOTION_GAP = 60 # second
 
 
 class DoorMotionDetection(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         
-#     def initialize(self):
-#         try:
-#             GPIO.setmode(GPIO.BCM)
-#             GPIO.setwarnings(False)
-#             GPIO.setup(LED, GPIO.OUT)
-#             GPIO.setup(MOTION, GPIO.IN, pull_up_down=GPIO.PUD_down)  # enable the pull-up
-#             GPIO.add_event_detect(MOTION, self.handle_motion(), GPIO.RISING, bouncetime=500) # increase the bouncetime to avoid the event frequently detection
-#         except GPIO.error:
-#             self.log.warning("Can't initialize GPIO - skill will not load")
-#             self.speak_dialog("error.initialise") # create the error.initialise.dialog file
-#         finally:
-#             self.schedule_repeating_event(self.handle_motion(),
-#                                           None, 0.1, 'motion')
+    def initialize(self):
+        try:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
+            GPIO.setup(LED, GPIO.OUT)
+            GPIO.setup(MOTION, GPIO.IN, pull_up_down=GPIO.PUD_down)  # enable the pull-up
+            GPIO.add_event_detect(MOTION, self.handle_motion(), GPIO.RISING, bouncetime=500) # increase the bouncetime to avoid the event frequently detection
+        except GPIO.error:
+            self.log.warning("Can't initialize GPIO - skill will not load")
+            self.speak_dialog("error.initialise") # create the error.initialise.dialog file
+        finally:
+            self.schedule_repeating_event(self.handle_motion(),
+                                          None, 0.1, 'motion')
             
-#         def handle_motion(self, message):
-#         if GPIO.event_detected(MOTION):
-#             now = time.time()           # catch the current time
+        def handle_motion(self, message):
+        if GPIO.event_detected(MOTION):
+            now = time.time()           # catch the current time
 
-#             """detect_gap is a time gap between the last motion and the current. If it is first then zero.
-#             Avoiding the first motion to makesure """
-#             detect_gap = now - detect_time[-1] if len(detect_time) >= 1 else now
-#             detect_time.append(now)  # append the time in the list
+            """detect_gap is a time gap between the last motion and the current. If it is first then zero.
+            Avoiding the first motion to makesure """
+            detect_gap = now - detect_time[-1] if len(detect_time) >= 1 else now
+            detect_time.append(now)  # append the time in the list
 
-#             if detect_gap == MOTION_GAP:
-#                 play_wav(REMINDER_PING)  # play the beep
-#                 self.speak('I still can see the motion at your door')
-#                 detect_time.clear()
+            if detect_gap == MOTION_GAP:
+                play_wav(REMINDER_PING)  # play the beep
+                self.speak('I still can see the motion at your door')
+                detect_time.clear()
 
-#             if detect_gap >= 10:
-#                 play_wav(REMINDER_PING)  # play the beep
-#                 self.speak_dialog("DoorDetectionDialog")  # need to create a .dialog file
-#                 self.log.info("GPIO.event_detected")
+            if detect_gap >= 10:
+                play_wav(REMINDER_PING)  # play the beep
+                self.speak_dialog("DoorDetectionDialog")  # need to create a .dialog file
+                self.log.info("GPIO.event_detected")
 
-#             self.remove_event_detect(MOTION)    # clear the event
+            self.remove_event_detect(MOTION)    # clear the event
             
             
 
