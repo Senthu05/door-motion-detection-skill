01fcc7fe3d8bf03a951f67107bf4c4b7af557817
Senthuran Puthirarajah <69337831+Senthu05@users.noreply.github.com>
2022-03-22 17:23:09 +1030

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,42 +18,42 @@ class DoorMotionDetection(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    def initialize(self):
+#     def initialize(self):
 
-        try:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setwarnings(False)
-            GPIO.setup(LED, GPIO.OUT)
-            GPIO.setup(MOTION, GPIO.IN)  # enable the pull-up ( pull_up_down=GPIO.PUD_down)
-            GPIO.add_event_detect(MOTION, self.handle_motion(), GPIO.RISING,
-                                  bouncetime=500)  # increase the bouncetime to avoid the event frequently detection
-        except GPIO.error:
-            self.log.warning("Can't initialize GPIO - skill will not load")
-            self.speak_dialog("error.initialise")  # create the error.initialise.dialog file
-        finally:
-            self.schedule_repeating_event(self.handle_motion(),
-                                          None, 0.1, 'motion')
-#             self.register_intent(detection.motion.door, self.handle_detection_motion_door)
-#             my_setting = self.settings.get('my_setting')
+#         try:
+#             GPIO.setmode(GPIO.BCM)
+#             GPIO.setwarnings(False)
+#             GPIO.setup(LED, GPIO.OUT)
+#             GPIO.setup(MOTION, GPIO.IN)  # enable the pull-up ( pull_up_down=GPIO.PUD_down)
+#             GPIO.add_event_detect(MOTION, self.handle_motion(), GPIO.RISING,
+#                                   bouncetime=500)  # increase the bouncetime to avoid the event frequently detection
+#         except GPIO.error:
+#             self.log.warning("Can't initialize GPIO - skill will not load")
+#             self.speak_dialog("error.initialise")  # create the error.initialise.dialog file
+#         finally:
+#             self.schedule_repeating_event(self.handle_motion(),
+#                                           None, 0.1, 'motion')
+# #             self.register_intent(detection.motion.door, self.handle_detection_motion_door)
+# #             my_setting = self.settings.get('my_setting')
 
-    def handle_motion(self, message):
-        if GPIO.event_detected(MOTION):
-            now = time.time()  # catch the current time
+#     def handle_motion(self, message):
+#         if GPIO.event_detected(MOTION):
+#             now = time.time()  # catch the current time
 
-            """detect_gap is a time gap between the last motion and the current. If it is first then zero.
-            Avoiding the first motion to makesure """
-            detect_gap = now - detect_time[-1] if len(detect_time) >= 1 else now
-            detect_time.append(now)  # append the time in the list
+#             """detect_gap is a time gap between the last motion and the current. If it is first then zero.
+#             Avoiding the first motion to makesure """
+#             detect_gap = now - detect_time[-1] if len(detect_time) >= 1 else now
+#             detect_time.append(now)  # append the time in the list
 
-            if detect_gap == MOTION_GAP:
-                self.speak('I still can see the motion at your door')
-                detect_time.clear()
+#             if detect_gap == MOTION_GAP:
+#                 self.speak('I still can see the motion at your door')
+#                 detect_time.clear()
 
-            if detect_gap >= 10:
-                self.speak_dialog("DoorDetectionDialog")  # need to create a .dialog file
-                self.log.info("GPIO.event_detected")
+#             if detect_gap >= 10:
+#                 self.speak_dialog("DoorDetectionDialog")  # need to create a .dialog file
+#                 self.log.info("GPIO.event_detected")
 
-            self.remove_event_detect(MOTION)  # clear the event
+#             self.remove_event_detect(MOTION)  # clear the event
 
     @intent_file_handler('detection.motion.door.intent')
     def handle_detection_motion_door(self, message):
