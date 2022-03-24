532c0f6cda2de7985ee195df27ac2b1988d6e327
Senthuran Puthirarajah <69337831+Senthu05@users.noreply.github.com>
2022-03-24 12:29:04 +1030

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@ class DoorMotionDetection(MycroftSkill):
             GPIO.setmode(GPIO.BCM)
             GPIO.setwarnings(False)
             GPIO.setup(LED, GPIO.OUT)
-            GPIO.setup(MOTION, GPIO.IN, pull_up_down=GPIO.PUD_down)  # enable the pull-up ( pull_up_down=GPIO.PUD_down)
+            GPIO.setup(MOTION, GPIO.IN)  # enable the pull-up ( pull_up_down=GPIO.PUD_down)
 #             GPIO.remove_event_detect(BUTTON)
 #             GPIO.add_event_detect(MOTION, GPIO.RISING, bouncetime=500)  # increase the bouncetime to avoid the event frequently detection
             
