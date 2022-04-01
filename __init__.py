a99a930eb73e344b2b9e66dadf219bf60bd853a1
Senthuran Puthirarajah <69337831+Senthu05@users.noreply.github.com>
2022-04-01 17:08:29 +1030

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,16 +11,15 @@ from mycroft.util.format import (nice_date, nice_duration, nice_time,
                                  date_time_format)
 from mycroft.util.time import now_utc, to_local, now_local
 
-REMINDER_PING = join(dirname(__file__), 'twoBeep.wav') # NOT WORKING
+REMINDER_PING = join(dirname(__file__), 'twoBeep.wav') 
 
 # GPIO pins
 MOTION = 18
 LED = 25
-record_list = []
+
+record_list = [] # record the bell time 
 Bell_GAP_1 = 15.0  # second
 Bell_GAP_2 = 120.0
-
-# Bell_GAP = 0:02:00.00
 list_clear = 3600  # list will be clear data more than an hour
 
 
@@ -30,8 +29,6 @@ class DoorMotionDetection(MycroftSkill):
 
     def initialize(self):
         my_setting = self.settings.get('my_setting')
-        self.log.info("initiliazing")
-
         try:
             GPIO.setmode(GPIO.BCM)
             GPIO.setwarnings(False)
@@ -40,8 +37,6 @@ class DoorMotionDetection(MycroftSkill):
             GPIO.remove_event_detect(MOTION)
             GPIO.add_event_detect(MOTION, GPIO.RISING,
                                   bouncetime=500)  # increase the bouncetime to avoid the event frequently detection
-
-
         except:
             self.log.warning("Can't initialize GPIO - skill will not load")
             self.speak_dialog("error.initialize")  # create the error.initialise.dialog file
@@ -49,28 +44,16 @@ class DoorMotionDetection(MycroftSkill):
         finally:
             self.schedule_repeating_event(self.handle_motion,
                                           None, 0.1, 'check_motion')
-            # self.register_intent(detection.motion.door.intent, self.handle_detection_motion_door)
-
 
     def handle_motion(self, message):
         if GPIO.event_detected(MOTION):
             now = now_local()  # catch the current time in the registered location 
-            #record_list.append(now)
             #next_bell_gap = now - record_list[-1] if len(record_list) >= 1 else now  # calculate the gap   
             if len(record_list) >= 1:
                 next_bell_gap = now_local() - record_list[-1]
-                self.log.info("I am at if condition")
             else:
                 next_bell_gap = now - now
-                self.log.info("I am at else condition")
-            self.log.info("next_bell_gap = ")
-            self.log.info(next_bell_gap)
             bell_gap_sec = next_bell_gap.total_seconds() # convert to seconds
-            
-           # bell_gap_sec = next_bell_gap / timedelta(seconds=1)  # convert to seconds
-
-            self.log.info("time gap")
-            self.log.info(bell_gap_sec)
 
             if bell_gap_sec > Bell_GAP_2:
                 play_wav(REMINDER_PING)
@@ -90,11 +73,6 @@ class DoorMotionDetection(MycroftSkill):
 
             if len(record_list) > 5:  # remove the list if more than 5 record
                 record_list.pop(0)
-
-            #             for x in record_list:
-
-    #                 if (now - x) > list_clear:
-    #                     record_list.clear()
 
     @intent_file_handler('detection.motion.door.intent')
     def handle_detection_motion_door(self, message):
