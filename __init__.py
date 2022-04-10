7034161ec8b77b5cd702f2b964ef141e056da709
Senthuran Puthirarajah <69337831+Senthu05@users.noreply.github.com>
2022-04-11 08:48:07 +0930

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -46,8 +46,7 @@ class DoorMotionDetection(MycroftSkill):
     def handle_motion(self, message):
         if GPIO.event_detected(MOTION):
             now = now_local()  # catch the current time in the registered location
-           # next_bell_gap = now - (record_list[-1] if len(record_list) >= 1 else now)  # calculate the gap
-            next_bell_gap = now - record_list[-1] 
+            next_bell_gap = now - (record_list[-1] if len(record_list) >= 1 else now)  # calculate the gap
             bell_gap_sec = next_bell_gap.total_seconds() # convert to seconds
 
             if bell_gap_sec > Bell_GAP_2:
