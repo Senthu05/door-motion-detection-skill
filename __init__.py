5a1fb23f72e8da1ca0f9f5299bd77f57ab61b0a7
Senthuran Puthirarajah <69337831+Senthu05@users.noreply.github.com>
2022-06-09 13:44:24 +0930

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,96 +1,188 @@
 import time
 import datetime
 import RPi.GPIO as GPIO
+import re
 
-from os.path import dirname, join
 from mycroft import MycroftSkill, intent_file_handler
 from datetime import datetime, timedelta
-from mycroft.messagebus.message import Message
-from mycroft.util import play_wav
-from mycroft.util.format import (nice_date, nice_duration, nice_time,
-                                 date_time_format)
 from mycroft.util.time import now_utc, to_local, now_local
+from mycroft.util.format import join_list
+
+sensor_pin = [19, 20, 21, 22, 23, 24]  # define the sensor pins
+LED = 25  # LED output
+record_dic = {}  # record the motion
+sensor_room = ["living"]  # room name need to be assigned from the site
+bed_time = "210000"
+wake_time = "060000"
+first_check_time = timedelta(seconds=60)  # how frequently check the motion
+second_check_time = 20.0  # how long wait after no respond for first check
+no_respond_flag = True
+no_email_flag = True
+more_question_handler_flag = True
 
 
-REMINDER_PING = join(dirname(__file__), 'twoBeep.wav')
-MOTION = 18   # GPIO pins
-LED = 25
-record_list = []       #record the bell time
-Bell_GAP_1 = 15.0  # second
-Bell_GAP_2 = 120.0
-list_clear = 3600  # list will be clear data more than an hour
-
-
-class DoorMotionDetection(MycroftSkill):
+class HomecareWithMotion(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.immediate_help = ["slipped", "fell down", "hurt", "bleeding", "hit"]
+        self.ask_to_inform = ["cannot walk", "can't walk", "can't wake up", "cannot wake up"]
+        self.medicine = ["headache", "fever", "stomach"]
+        self.inform = ["call", "inform", "email", "ring"]
+        self.more_question = ["i don't know", "I have no idea"]
+        self.make_fun = ["lazy", "lonely"]
+        self.angry = ["shut your mouth", "shut up", "stop"]
 
     def initialize(self):
         my_setting = self.settings.get('my_setting')
+        # show_time = self.settings.get('show_time', False)
         try:
             GPIO.setmode(GPIO.BCM)
             GPIO.setwarnings(False)
             GPIO.setup(LED, GPIO.OUT)
-            GPIO.setup(MOTION, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # enable the pull-up ( pull_up_down=GPIO.PUD_down)
-            GPIO.remove_event_detect(MOTION)
-            GPIO.add_event_detect(MOTION, GPIO.RISING,
-                                  bouncetime=500)  # increase the bouncetime to avoid the event frequently detection
+            for x in range(len(sensor_room)):  # loop run for every room
+                GPIO.setup(sensor_pin[x], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # enable the pull-up
+                GPIO.remove_event_detect(sensor_pin[x])
+                GPIO.add_event_detect(sensor_pin[x], GPIO.RISING,
+                                      bouncetime=500)  # high bounce time to avoid the event frequently detection
+
         except:
             self.log.warning("Can't initialize GPIO - skill will not load")
             self.speak_dialog("error.initialize")  # create the error.initialise.dialog file
 
         finally:
             self.schedule_repeating_event(self.handle_motion,
-                                          None, 0.1, 'check_motion')
+                                          None, 20, name='check_motion')
+            record_dic["time loaded"] = now_local()
 
     def handle_motion(self, message):
-        if GPIO.event_detected(MOTION):
-            now = now_local()  # catch the current time in the registered location
-            self.log.info("current time")
-            self.log.info(now)
-            next_bell_gap = now - (record_list[-1] if len(record_list) >= 1 else now)  # calculate the gap
-            self.log.info("gap")
-            self.log.info(next_bell_gap)
-            bell_gap_sec = next_bell_gap.total_seconds() # convert to seconds
+        global record_dic
+        for x in range(len(sensor_room)):  # check all sensors
+            if GPIO.event_detected(sensor_pin[x]):
+                self.log.info("event detected")
+                event_time = now_local()
+                record_dic["time loc" + str(x)] = event_time
+                self.cancel_scheduled_event('no_respond')
+        # get all the values which match the key start with "time" used regex pattern match
+        time_list = [v for k, v in record_dic.items() if bool(re.match("time", k))]
+        now = now_local()
+        gap = timedelta(seconds=65)  # random value
+        for y in range(len(time_list)):
+            temp_gap = now - time_list[y]
+            if temp_gap < gap:
+                gap = temp_gap
+        gap_second = gap.total_seconds()  # convert the gap in second
+        bed_timeHour = datetime.strptime(bed_time, "%H%M%S").time()
+        wake_timeHour = datetime.strptime(wake_time, "%H%M%S").time()
+        current_hour = now.time()
+        if gap_second > first_check_time.total_seconds() and (wake_timeHour < current_hour < bed_timeHour):
+            self.log.info(time_list)
+            record_dic.clear()  # clear the dictionary
+            record_dic["time interaction"] = now_local()  # record the time (must, to check the different)
+            self.conversation()
 
-            if bell_gap_sec > Bell_GAP_2:
-                self.bell_ring(3)
-                self.speak_dialog("First.Bell")
-                record_list.append(now)  # append the time in the list
-                #record_list.append([now, day])  # append the time in the list
-                
-            if bell_gap_sec < Bell_GAP_1:
-                if len(record_list) == 0:
-                  record_list.append(now)
-                  #record_list.append([now, day])                 
-                self.bell_ring(3)
-                self.speak_dialog("First.Bell")
-                
-            if Bell_GAP_1 <= bell_gap_sec <= Bell_GAP_2:
-                self.bell_ring(2)
-                self.speak_dialog("Next.Bell")
-                
-            if len(record_list) > 5:  # remove the list if more than 5 record
-                record_list.pop(0)
+    @intent_file_handler('motion.with.homecare.intent')
+    def handle_motion_with_homecare(self, message):
+        resp = self.get_response("inform")
+        title = "no help needed"
+        self.mail(title, resp)
+        self.speak_dialog('motion.with.homecare')
 
-    def bell_ring(self, number):
-        for x in range(number):
-            play_wav(REMINDER_PING)
-        time.sleep(.4)
-
-    @intent_file_handler('detection.motion.door.intent')
-    def handle_detection_motion_door(self, message):
-        dt = record_list[-1]  # get the last value of list
-        s = nice_time(dt)
-        self.log.info("nice time")
-        self.log.info(s)
-        if(dt.date()== now_local().date()):
-            self.speak_dialog("today")
-            self.speak_dialog('detection.motion.door', {"time": s})
+    def conversation(self):
+        resp = self.get_response("motion.confirmation")
+        if self.voc_match(resp, 'yes'):
+            self.speak_dialog("no.help.confirmation")
+        elif self.voc_match(resp, 'no'):
+            no_resp = self.get_response("how.can.i.help")
+            if self.voc_match(no_resp, "no"):
+                title = "help requested"
+                body = "Hi, identified that system con not help "
+                self.mail(title, body)
+            else:
+                self.help(no_resp)
+        elif resp is not None:
+            self.help(resp)
         else:
-            self.speak_dialog('detection.motion.door', {"time": s})
-        
+            global no_respond_flag
+            no_respond_flag = not no_respond_flag
+            if no_respond_flag:
+                title = "no respond"
+                body = "Hi,\nI couldn't reach the resident after 2 attempt"
+                self.send_email(title, body)
+            else:
+                self.schedule_event(self.conversation, second_check_time, name="no_respond")
+
+    def mail(self, title, utterance):
+        body = "Hi,\n This is the respond I received \" " + utterance + " \""
+        self.send_email(title, body)
+
+    def help(self, response):
+        global no_email_flag
+        no_email_flag = not no_email_flag  # if user  say not to inform even user not well just wait only one time
+
+        immediate_help_words = [a for a in self.immediate_help if a in response]
+        ask_to_inform_words = [a for a in self.ask_to_inform if a in response]
+        medicine_words = [a for a in self.medicine if a in response]
+        inform_words = [a for a in self.inform if a in response]
+        more_question_words = [a for a in self.more_question if a in response]
+        make_fun_words = [a for a in self.make_fun if a in response]
+        angry_words = [a for a in self.angry if a in response]
+
+        if len(immediate_help_words) > 0:
+            self.speak_dialog("immediate.help", data={'immediate': join_list(immediate_help_words, "and")})
+            title = "immediate help"
+            self.mail(title, response)
+        elif len(ask_to_inform_words) > 0:
+            yes_no = self.ask_yesno("ask.to.inform", data={'ask': join_list(ask_to_inform_words, "and")})
+            if yes_no == "yes":
+                title = ask_to_inform_words[0]
+                self.mail(title, response)
+            elif yes_no == "no":
+                if no_email_flag:
+                    title = "requested rejected to email 2 times"
+                    self.mail(title, response)
+                else:
+                    self.speak("ok, just ask me if you need to contact")
+            else:
+                title = ask_to_inform_words[0]
+                self.mail(title, response)
+            # need to handle else as schedule  event or email
+        elif len(medicine_words) > 0:
+            title = medicine_words[0]
+            yes_no = self.ask_yesno("medicine", data={})
+            if yes_no == "no":
+                title = "Did not get medicine"
+                self.speak("You better get medicine now, It might be the reason")
+            elif yes_no == "yes":
+                self.speak("good to hear that you got medicine")
+                title = medicine_words[0]
+            self.mail(title, response)
+        elif len(inform_words) > 0:
+            resp = self.get_response("inform")
+            title = "need to contact"
+            self.mail(title, resp)
+            self.speak("Ok informing")
+        elif len(more_question_words) > 0:
+            self.schedule_event(self.more_question_handler, 1.0, name="more_question")
+        elif len(make_fun_words) > 0:
+            self.speak_dialog("make.fun", data={'fun': join_list(immediate_help_words, "or")})
+        elif len(angry_words) > 0:
+            self.speak_dialog("angry")
+        else:
+            self.speak_dialog("else.help")
+            title = "confuse conversation"
+            self.mail(title, response)
+
+    def more_question_handler(self):
+        global more_question_handler_flag
+        more_question_handler_flag = not more_question_handler_flag
+        if more_question_handler_flag:
+            title = "need some help"
+            body = "Hi \nResident is confusing, you better check"
+            self.send_email(title, body)
+        else:
+            resp = self.get_response("more.question")
+            self.help(resp)
+
 
 def create_skill():
-    return DoorMotionDetection()
-
+    return HomecareWithMotion()
